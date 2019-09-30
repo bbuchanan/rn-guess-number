99bd495e0a0f4aceb7eede8972773ea5fa856cc6
Bill Buchanan <bbuchanan@gmail.com>
2019-09-30 16:29:41 -0400

    89. Using dimensions in 'if' checks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,8 +5,8 @@ import {
   StyleSheet,
   Button,
   Alert,
-  ScrollView,
-  FlatList
+  FlatList,
+  Dimensions
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -120,7 +120,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%"
   },
@@ -140,7 +140,7 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    width: "60%"
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%"
   }
 });
 
