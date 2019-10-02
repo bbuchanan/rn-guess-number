9090e1ef948fb3a439050524fb9014211da11b57
Bill Buchanan <bbuchanan@gmail.com>
2019-10-02 11:48:41 -0400

    101. Using SafeAreaView

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,8 +5,7 @@ import {
   Image,
   StyleSheet,
   Dimensions,
-  ScrollView,
-  SafeAreaView
+  ScrollView
 } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
@@ -16,31 +15,29 @@ import Colors from "../constants/colors";
 
 const GameOverScreen = props => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.screen}>
-          <TitleText>The Game is Over!</TitleText>
-          <View style={styles.imageContainer}>
-            <Image
-              style={styles.image}
-              source={require("../assets/success.png")}
-              // source={{
-              //   uri: "https://i.stack.imgur.com/AeeD3.png"
-              // }}
-              resizeMode="cover"
-            />
-          </View>
-          <View style={styles.resultContainer}>
-            <BodyText style={styles.resultText}>
-              Your phone needed{" "}
-              <Text style={styles.highlight}>{props.rounds}</Text> to guess the
-              number <Text style={styles.highlight}>{props.userNumber}</Text>
-            </BodyText>
-          </View>
-          <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/success.png")}
+            // source={{
+            //   uri: "https://i.stack.imgur.com/AeeD3.png"
+            // }}
+            resizeMode="cover"
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{" "}
+            <Text style={styles.highlight}>{props.rounds}</Text> to guess the
+            number <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
   );
 };
 
